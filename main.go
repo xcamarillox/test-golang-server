0acd948c97eb7b3ea8154592de912adf0ae9a17a
Carlos Camarillo <camarillo@Carloss-MacBook-Pro.local>
2022-07-29 19:37:52 -0500

    HTTP GET method with specific ID in subroute retriving the specified ID.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -41,12 +44,66 @@ var availableCars = []CarSpecs{
 	},
 }
 
+func checkIfCarIsFound(id int) (bool, int) {
+	for index, car := range availableCars {
+		if car.Id == id {
+			return true, index
+		}
+	}
+	return false, 0
+}
+
+func checkIfIdIsCorrect(id string) (int, error) {
+	idInt, err := strconv.Atoi(id)
+	return idInt, err
+}
+
 func main() {
 
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(availableCars)
 	})
-
+	app.Post("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(availableCars)
+	})
+	app.Get("/:id", func(c *fiber.Ctx) error {
+		var existingId bool
+		var carIndex int
+		idInt, err := checkIfIdIsCorrect(c.Params("id"))
+		if err == nil {
+			existingId, carIndex = checkIfCarIsFound(idInt)
+		}
+		if err == nil && existingId {
+			return c.Status(fiber.StatusOK).JSON(availableCars[carIndex])
+		}
+		return c.SendStatus(404)
+	})
+	app.Put("/:id", func(c *fiber.Ctx) error {
+		var existingId bool
+		var carIndex int
+		idInt, err := checkIfIdIsCorrect(c.Params("id"))
+		if err == nil {
+			existingId, carIndex = checkIfCarIsFound(idInt)
+		}
+		if err == nil && existingId {
+			//return c.Status(fiber.StatusOK).JSON(availableCars[carIndex])
+			fmt.Println(carIndex)
+		}
+		return c.SendStatus(404)
+	})
+	app.Delete("/:id", func(c *fiber.Ctx) error {
+		var existingId bool
+		var carIndex int
+		idInt, err := checkIfIdIsCorrect(c.Params("id"))
+		if err == nil {
+			existingId, carIndex = checkIfCarIsFound(idInt)
+		}
+		if err == nil && existingId {
+			//return c.Status(fiber.StatusOK).JSON(availableCars[carIndex])
+			fmt.Println(carIndex)
+		}
+		return c.SendStatus(404)
+	})
 	app.Listen(":3000")
 }
