0fd0628ea8b66bb2ed8e615158e4f2b64b5e589e
Carlos Camarillo <carlos.adrian.camarillo@gmail.com>
2022-08-01 16:44:25 -0500

    More specific on error and success messages. JSON tags changed to camelCase. Year in car must be positive, validated in a function. Refactor.

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 )
 
 type CarSpecs struct {
-	Id               int
-	Make             string
-	Model            string
-	Year             int
-	EngineCapacity   float32 //enlitros
-	Color            string
-	TransmissionType string // manual / automatica
-	FuelType         string //gasolina / diesel
-	IsBrandNew       bool
+	Id               int     `json:"id"`
+	Make             string  `json:"make"`
+	Model            string  `json:"model"`
+	Year             int     `json:"year"`
+	EngineCapacity   float32 `json:"engineCapacity"` //enlitros
+	Color            string  `json:"color"`
+	TransmissionType string  `json:"transmissionType"` // manual / automatica
+	FuelType         string  `json:"fuelType"`         //gasolina / diesel
+	IsBrandNew       bool    `json:"isBrandNew"`
 }
 
 var availableCars = []CarSpecs{
@@ -70,48 +70,69 @@ func getNewIntId() int {
 	}
 	return len(availableCars)
 }
-
+func getMeAReponseAndANewCarWithCarData(c *fiber.Ctx, mode string, carIndex int) (int, CarSpecs) {
+	newCar := CarSpecs{}
+	if mode == "checkDataGetMeACar" || mode == "checkDataAndCarIndexGetMeACar" {
+		if err := c.BodyParser(&newCar); err != nil {
+			c.SendString("La estructura de los datos recibidos es incorrecta.")
+			return 400, newCar // Error code 400
+		}
+		if newCar.Year < 0 {
+			c.SendString("El año de manufactura del automóvil no puede ser negativo.")
+			return 400, newCar // Error code 400
+		}
+	}
+	if mode == "checkCarIndex" || mode == "checkDataAndCarIndexGetMeACar" {
+		if carIndex < 0 {
+			c.SendString("El ID enviado en la ruta es incorrecto o no fue encontrado.")
+			return 404, newCar // Error code 404
+		}
+	}
+	return 0, newCar //incoming data is ok and setted in car, 0 returned as no error code
+}
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(availableCars)
 	})
 	app.Post("/", func(c *fiber.Ctx) error {
-		newCar := CarSpecs{}
-		if err := c.BodyParser(&newCar); err != nil {
-			return c.SendStatus(400)
+		responseCode, newCar := getMeAReponseAndANewCarWithCarData(c, "checkDataGetMeACar", 0)
+		if responseCode != 0 {
+			return c.SendStatus(responseCode)
 		}
 		newCar.Id = getNewIntId()
 		availableCars = append(availableCars, newCar)
-		return c.SendStatus(202)
+		c.SendString("El recurso fue añadido con exito.")
+		return c.SendStatus(201)
 	})
 	app.Get("/:id", func(c *fiber.Ctx) error {
 		carIndex, _ := getIndexOfStringId(c.Params("id"))
-		if carIndex < 0 {
-			return c.SendStatus(404)
+		responseCode, _ := getMeAReponseAndANewCarWithCarData(c, "checkCarIndex", carIndex)
+		if responseCode != 0 {
+			return c.SendStatus(responseCode)
 		}
 		return c.Status(fiber.StatusOK).JSON(availableCars[carIndex])
 	})
 	app.Put("/:id", func(c *fiber.Ctx) error {
 		carIndex, idInt := getIndexOfStringId(c.Params("id"))
-		if carIndex < 0 {
-			return c.SendStatus(404)
-		}
-		carToEdit := CarSpecs{}
-		if err := c.BodyParser(&carToEdit); err != nil {
-			return c.SendStatus(400)
+		responseCode, carToEdit := getMeAReponseAndANewCarWithCarData(c, "checkDataAndCarIndexGetMeACar", carIndex)
+		if responseCode != 0 {
+			return c.SendStatus(responseCode)
 		}
 		availableCars[carIndex] = carToEdit
 		availableCars[carIndex].Id = idInt
+		c.SendString("El recurso fue editado con exito.")
 		return c.SendStatus(202)
 
 	})
 	app.Delete("/:id", func(c *fiber.Ctx) error {
 		carIndex, _ := getIndexOfStringId(c.Params("id"))
-		if carIndex < 0 {
-			return c.SendStatus(404)
+		responseCode, _ := getMeAReponseAndANewCarWithCarData(c, "checkCarIndex", carIndex)
+		if responseCode != 0 {
+			return c.SendStatus(responseCode)
 		}
 		availableCars = append(availableCars[:carIndex], availableCars[carIndex+1:]...)
+		c.SendString("El recurso fue eliminado con exito.")
 		return c.SendStatus(202)
 	})
 	app.Listen(":3000")
